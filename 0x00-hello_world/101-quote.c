e9657be375c57dda3c551c1fd49ed002f612f562
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-06-15 18:37:44 -0700

    My fifth commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Descripiton: A C program that prints with function instead of printf and puts
+ * Descripiton: A C program that prints with write function
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
