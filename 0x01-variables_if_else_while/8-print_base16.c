0cebd134bdd93d25cc5e891377e689d592d3843b
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-06-16 20:00:30 -0700

    My sixth commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A C program that prints the alphabet
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	char ch;
+
+	n = 48;
+	char = 'a';
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	while (ch <= 'f')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
