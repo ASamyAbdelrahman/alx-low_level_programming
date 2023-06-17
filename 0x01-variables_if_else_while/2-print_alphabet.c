c82dc7a0bdb664323e00e8290a8cafbc6f61ca52
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-06-16 19:12:24 -0700

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A C program that prints the alphabet
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+}
+
