fcb89d49847002d09067ed032fd1164dff592754
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-06-17 01:19:06 -0700

    Cool

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A C program that prints two two-digit numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int firstdigit, seconddigit, thirddidgit;
+
+	firstdigit = 0;
+	while (firstdigit <= 99)
+	{
+		seconddigit = firstdigit;
+		while (seconddigit <= 99)
+		{
+			if (seconddigit != firstdigit)
+			{
+				putchar((firstdigit / 10) + 48);
+				putchar((firstdigit % 10) + 48);
+				putchar(' ');
+				putchar((seconddigit / 10) + 48);
+				putchar((seconddigit % 10) + 48);
+				if (firstdigit != 98 || seconddigit != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			seconddigit++;
+		}
+		firstdigit++;
+	}
+	putchar('\n');
+	return (0);
+}
