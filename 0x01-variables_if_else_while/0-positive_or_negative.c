57a1b0e1488ec0e4d026ff750555f2bd144e1fe4
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-06-16 18:05:37 -0700

    My sixth commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: A C program to determine the sign of the number.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("is positive");
+	else if (n == 0)
+		printf("is zero");
+	else
+		printf("is negative");
+	return (0);
+}
