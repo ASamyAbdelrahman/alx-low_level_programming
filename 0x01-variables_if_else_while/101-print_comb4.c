369bc0600f93ae0a2d6c5255ce240a97041422c6
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-06-16 22:56:12 -0700

    Cool

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A C program that prints the alphabet
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n1, n2, n3, i, j, z;
+
+	i = 1;
+	z = 1;
+	for (n1 = 48; n1 <= 57; n1++)
+	{
+		n2 = 48;
+		n2 = n2 + i;
+		j = 1;
+		for (; n2 <= 57; n2++)
+		{
+			n3 = 48;
+			n3 = n3 + j + z;
+			for (; n3 <= 57; n3++)
+			{
+				putchar(n1);
+				putchar(n2);
+				putchar(n3);
+				if (i != 8 && z != 9 && j != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				j++;
+			}
+			z++;
+			i++;
+		}
+		putchar('\n');
+		return (0);
+}
