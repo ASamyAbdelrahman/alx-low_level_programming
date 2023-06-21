c8849fa7a810c62b9e09ab2c9473973645b1d981
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-06-20 19:11:11 -0700

    cool

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,14 @@ int print_last_digit(int digit)
 {
 	int last_digit;
 
-	last_digit = digit % 10;
-	_putchar(last_digit + 48);
-	return (last_digit);
+	if (digit < 0)
+	{
+		last_digit = (-1 * digit) % 10;
+		_putchar(lastdigit + 48);
+	}
+	else
+	{
+		last_digit = digit % 10;
+		_putchar(lastdigit + 48);
+	}
 }
