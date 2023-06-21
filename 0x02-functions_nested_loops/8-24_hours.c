b83ff3a877ad48f2282f6d52ad01ec0d27b6f85b
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-06-20 19:38:04 -0700

    cool

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,9 +5,11 @@
 
 void jack_bauer(void)
 {
-	for (int hr = 0; hr <= 23; hr++)
+	int hr, min;
+
+	for (hr = 0; hr <= 23; hr++)
 	{
-		for (int min = 0; min <= 59; min++)
+		for (min = 0; min <= 59; min++)
 		{
 			_putchar((hr / 10) + 48);
 			_putchar((hr % 10) + 48);
