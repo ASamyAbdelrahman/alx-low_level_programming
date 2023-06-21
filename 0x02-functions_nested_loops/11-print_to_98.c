6aee77a1e4bb25ed8b287372c8058bd04c90ae5e
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-06-20 21:29:14 -0700

    cool

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,36 +7,13 @@
 
 void print_to_98(int n)
 {
-	int i, add;
+	int i;
 
-	for (i = 0; i <= 98 - n; i++)
-	{
-		add = n + i;
-		if (add < -9)
-		{
-			_putchar('-');
-			_putchar(-1 * (add / 10) + 48);
-			_putchar(-1 * (add % 10) + 48);
-		}
-		if (add >= -9 && add < 0)
-		{
-			_putchar('-');
-			_putchar((-1 * add) + 48);
-		}
-		if (add >= 0)
-		{
-			if (add <= 9)
-			{
-				_putchar(add + 48);
-			}
-			else
-			{
-				_putchar((add / 10) + 48);
-				_putchar((add % 10) + 48);
-			}
-		}
-		_putchar(',');
-		_putchar(' ');
-		_putchar('\n');
-	}
+	if (n > 98)
+		for (i = n; i > 98; i--)
+			printf("%d, ", i);
+	else
+		for (i = n; i < 98; i++)
+			printf("%d, ", i);
+	printf("98\n");
 }
