2c9b1f6d0cf6c458e3bc36208bbb9f9d2c1dee1a
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-06-29 14:26:13 -0700

    Merge two arrays with the number n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two strings.
+ * @dest: append to string
+ * @src: append from string
+ * @n: the number of appending
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int length, i;
+	char *y = dest;
+
+	i = 0;
+	length = 0;
+	while (*y != '\0')
+	{
+		++y;
+		length++;
+	}
+	if (n > 98 - length)
+	{
+		for (i = 0; i < 98 - length; i++)
+		{
+			*y = src[i];
+			y++;
+		}
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			*y = src[i];
+			y++;
+		}
+	}
+	return (dest);
+}
