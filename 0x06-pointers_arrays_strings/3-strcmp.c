8057ae5c6020b9f914a3510781046c5d89095f4a
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-06-29 16:03:53 -0700

    Compares the two arrays

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int value, i;
+	int i;
 
-	value = 0;
 	i = 0;
-	if (s1[i] == '\0' && s2[i] == '\0')
-		value = 0;
-	else if (s1[i] != '\0' && s2[i] != '\0')
-		value = s1[i] - s2[i];
-
-	return (value);
+	while(s1[i] == s2[i])
+	{
+		if (s1[i] == '\0')
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (s1[i] - s2[i]);
 }
 
