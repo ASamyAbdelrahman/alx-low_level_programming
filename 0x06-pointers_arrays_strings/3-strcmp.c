4a2f82726aa4e39f144d2d4965829fe00e95ea3b
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-06-29 15:37:25 -0700

    Compares the two arrays

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings.
+ * @s1: the first array.
+ * @s2: the second array.
+ * Return: value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int value, i;
+
+	value = 0;
+	i = 0;
+	value = s1[i] - s2[i];
+	return (value);
+}
+
