bf50650f4e43d1bebdd31cef90f88d74cc9c998d
root <root@LAPTOP-LFKJQ0PM.localdomain>
2023-07-04 02:43:44 +0300

    Searches a string for any of a set of bytes.

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,8 +4,8 @@
  * *_strpbrk - searches a string for any of a set of bytes.
  * @s: the first string.
  * @accept: the second string.
- * Return: returns a pointer to the byte in s that matches one of the bytes in accept
- *                 or NULL if no such byte is found.
+ * Return: returns a pointer to the byte in s that matches
+ *         one of the bytes in accept or NULL if no such byte is found.
  */
 
 char *_strpbrk(char *s, char *accept)
