f27b31b4c32595adeed89765af97edb0d64d28ec
root <root@LAPTOP-LFKJQ0PM.localdomain>
2023-07-08 05:34:30 +0300

    Addition of numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers.
+ * @argc: count
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum;
+	char *c;
+
+	sum = 0;
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+		{
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
+		}
+		sum = sum + atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
