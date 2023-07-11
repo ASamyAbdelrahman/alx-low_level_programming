9d7f9ae4d7728c55e9cc7279ac0d536d15a47039
root <root@LAPTOP-LFKJQ0PM.localdomain>
2023-07-11 12:15:14 +0300

    Copies a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: the string that will be copied.
+ * Return: returns ptr or returns NULL if str = NULL.
+ */
+
+char *_strdup(char *str)
+{
+	int i, length;
+	char *y = str;
+	char *ptr;
+
+	while (*y != '\0')
+	{
+		y++;
+		length++;
+	}
+	ptr = (char *) malloc(length * sizeof(char));
+
+	if (length != 0)
+	{
+		if (ptr != NULL)
+		{
+			for (i = 0; i < length - 1; i++)
+			{
+				ptr[i] = str[i];
+			}
+		}
+		else
+			return (NULL);
+	}
+	else
+		return (NULL);
+	return (ptr);
+}
