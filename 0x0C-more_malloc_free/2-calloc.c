83ef5b46d7d340b7884cf838f3360461257437df
root <root@LAPTOP-LFKJQ0PM.localdomain>
2023-07-13 17:19:32 +0300

    allocates memory for an array, using malloc.

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,12 +10,15 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int *ptr;
+	unsigned int i;
+	char *ptr;
 
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		*(ptr + i) = 0;
 	return (ptr);
 }
