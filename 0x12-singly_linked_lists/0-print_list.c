aa055747bce12be1d44f98e66049873ad8c16099
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-07-29 02:56:45 +0300

    Prints all elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@ size_t print_list(const list_t *h)
 
 	for (count = 0; h; count++)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		printf("[%d] %s\n", h->len, h->str ? h->str : "(nil)");
 		h = h->next;
 	}
 	return (count);
