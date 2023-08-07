085be0f78fcc276cace056d428452e405941f138
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-08-08 02:52:26 +0300

    Creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,20 +14,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, length;
+	int fd, lenght;
 	ssize_t res_write;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
-		length = 0;
-		while (*(text_content + length) == '\0')
-			length++;
-		res_write = write(fd, text_content, length);
+		lenght = 0;
+		while (*(text_content + lenght) != '\0')
+			lenght++;
+		res_write = write(fd, text_content, lenght);
 		if (res_write == -1)
 		{
 			write(1, "fails", 6);
